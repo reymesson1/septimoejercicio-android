287d620a841406e306a92c8be6e64754fe6dc435
reymesson1 <reymesson1>
2019-01-31 19:13:15 -0400

    working with kotlin

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var email = findViewById<EditText>(R.id.email)
+
+        var password = findViewById<EditText>(R.id.password)
+
+        var button = findViewById<Button>(R.id.button)
+
     }
+
+    
+
 }
